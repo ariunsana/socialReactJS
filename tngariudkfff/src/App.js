c3d456e5ac55c9aed3c298aa16935c132416fc3f
Sanaa <112365595+sw21d030@users.noreply.github.com>
2022-10-05 02:27:59 +0800

    profile

--- tngariudkfff/src/App.js
+++ tngariudkfff/src/App.js
@@ -1,8 +1,9 @@
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
+import Profile from "./pages/Profile/Profile";
 // import { Person } from "@mui/icons-material";
 function App() {
-  return <Home />;
+  return <Profile/>;
 }
 
 export default App;
