9b57cbfe88e61215864f29d0e612be130405eec7
Sanaa <112365595+sw21d030@users.noreply.github.com>
2022-10-05 03:05:56 +0800

    register

--- tngariudkfff/src/App.js
+++ tngariudkfff/src/App.js
@@ -2,9 +2,10 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/Profile/Profile";
+import Register from "./pages/register/Register";
 // import { Person } from "@mui/icons-material";
 function App() {
-  return <Login/>;
+  return <Register/>;
 }
 
 export default App;
