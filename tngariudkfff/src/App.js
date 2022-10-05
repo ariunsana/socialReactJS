d75b25b63ee493ab3340d4d10bf1c261ea0fd266
Sanaa <sw21d030@mandakh.edu.mn>
2022-10-04 18:27:07 -0700

    srf

--- tngariudkfff/src/App.js
+++ tngariudkfff/src/App.js
@@ -2,7 +2,7 @@ import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 // import { Person } from "@mui/icons-material";
 function App() {
-  return <Topbar />;
+  return <Home />;
 }
 
 export default App;
