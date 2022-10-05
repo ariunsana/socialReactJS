db561c60434072c57934330719ce9087e0c32986
Sanaa <sw21d030@mandakh.edu.mn>
2022-10-04 20:17:15 -0700

    s

--- tngariudkfff/src/components/rightbar/Rightbar.jsx
+++ tngariudkfff/src/components/rightbar/Rightbar.jsx
@@ -1,7 +1,28 @@
-import "./rightbar.css"
+import "./rightbar.css";
 
 export default function Rightbar() {
   return (
-    <div className="rightbar">Rightbar</div>
-  )
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Ariunsanaa</b> and <b>4 other friends</b> have a birthday today
+          </span>
+        </div>
+        <img src="assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightBarFriend">
+            <div className="rightbarProfileImgContainer">
+            <div className="rightbarProfileImg">
+              <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
