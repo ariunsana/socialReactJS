d5f827b04f3522775f55e94852a090ea3507e2f6
Sanaa <sw21d030@mandakh.edu.mn>
2022-10-04 19:02:20 -0700

    feeed

--- tngariudkfff/src/components/feed/Feed.jsx
+++ tngariudkfff/src/components/feed/Feed.jsx
@@ -1,7 +1,14 @@
+import Post from "../post/Post"
+import Share from "../share/Share"
 import "./feed.css"
 
 export default function Feed() {
   return (
-    <div className="feeds">feed</div>
+    <div className="feeds">
+      <div className="feedWrapper">
+        <Share/>
+        <Post/>
+      </div>
+    </div>
   )
 }
