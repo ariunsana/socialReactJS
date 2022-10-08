64a2da93f55103e82cd4de177752d82786023006
Sanaa <112365595+sw21d030@users.noreply.github.com>
2022-10-08 13:53:54 +0800

    search deeree l

---
+++ bichleg3/src/api.js
@@ -0,0 +1,15 @@
+export const geoAPIOptions= {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a640cc5ac6mshe6417bc876cf734p17be7ejsn32b9e9ccaa05',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+
+
+
+    
+    
--- bichleg3/src/components/search/Search.js
+++ bichleg3/src/components/search/Search.js
@@ -1,18 +1,35 @@
 import { useState } from "react";
-import {AsyncPaginate} from "react-select-async-paginate"
-const Search = ({onSearchChange}) =>{
+import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoAPIOptions } from '../../api';
+const Search = ({ onSearchChange }) => {
 
-    const[ search, setsearch]= useState(null);
+    const [search, setsearch] = useState(null);
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoAPIOptions)
+            .then((response) => response.json())
+            .then((response) => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch((err) => console.error(err));
+    }
     const handleOnChange = (searchData) => {
         setsearch(searchData);
         onSearchChange(searchData);
     }
-    return(
-        <AsyncPaginate 
-        placeholder="Search for city"
-        debounceTimeout={600}
-        value={search}
-        onChange={handleOnChange}
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
         />
     )
 }
