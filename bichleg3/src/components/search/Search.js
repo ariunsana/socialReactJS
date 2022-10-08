daab7e211b5c593de5fb45f2e8ce42441e368a24
Sanaa <112365595+sw21d030@users.noreply.github.com>
2022-10-08 13:06:57 +0800

    ehlel search

---
+++ bichleg3/src/components/search/Search.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import {AsyncPaginate} from "react-select-async-paginate"
+const Search = ({onSearchChange}) =>{
+
+    const[ search, setsearch]= useState(null);
+    const handleOnChange = (searchData) => {
+        setsearch(searchData);
+        onSearchChange(searchData);
+    }
+    return(
+        <AsyncPaginate 
+        placeholder="Search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        />
+    )
+}
+export default Search;
