25c65f64ecf6aa8c6a3814d8bc6c2350e2707648
Sanaa <112365595+sw21d030@users.noreply.github.com>
2022-10-10 02:00:08 +0800

    duussan

--- bichleg3/src/App.js
+++ bichleg3/src/App.js
@@ -3,6 +3,7 @@ import Search from './components/search/Search';
 import CurrentWeather from './components/current-weather/current-weather';
 import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import { useState } from 'react';
+import Forecast from './components/forecast/forecast';
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
@@ -19,7 +20,7 @@ function App() {
         const forecastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
-        forecast({ city: searchData.label, ...forecastResponse });
+        setForecast({ city: searchData.label, ...forecastResponse });
       })
       .catch((err) => console.log(err));
 
@@ -32,6 +33,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
     </div>
   );
 }
