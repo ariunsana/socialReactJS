be6a3f2f660c032b14ec191d24989a1bdeba4d79
Sanaa <112365595+sw21d030@users.noreply.github.com>
2022-10-10 02:49:45 +0800

    qwqewq

---
+++ cloud-app-react/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {MenuIcon, XIcon} from "@heroicons/react/outline"
+
+const Navbar=() => {
+  return (
+    <div>
+        <MenuIcon/>
+    </div>
+  )
+}
+
+export default Navbar;
