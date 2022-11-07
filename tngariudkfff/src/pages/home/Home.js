46e83b95491b7cb189ef9ad6718201a3922e42e0
sanaa <sw21d030@mandakh.edu.mn>
2022-11-07 12:51:09 +0800

    json arga daraan ynzalnaaa

--- tngariudkfff/src/pages/home/Home.js
+++ tngariudkfff/src/pages/home/Home.js
@@ -3,10 +3,40 @@ import Rightbar from "../../components/rightbar/Rightbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
 import "./home.css";
+import { useState , useEffect} from "react";
 
 export default function Home() {
+
+  const [datas, setDatas] = useState();
+  const urlLookup = "http://btax.mandakh.org:8000/lookup/";
+  useEffect(() => {
+    const bodyChiglel = {
+      action: "chiglel",
+    };
+    sendRequest(urlLookup, bodyChiglel);
+  }, []);
+
+  const sendRequest = async (url, body) => {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then(async (response) => {
+        setDatas(response);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+
   return (
     <>
+    <text>{JSON.stringify(datas)}</text>
       <Topbar />
       <div className="homeContainer">
         <Sidebar />
